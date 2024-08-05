a31cd3b0645d49482906855bc7186996a88ece81
covek01 <stojiljkovic.nikola2001@gmail.com>
2024-08-05 12:40:31 +0200

    initialized front without internal git

--- linguachat-front/README.md
+++ linguachat-front/README.md
@@ -1,4 +1,4 @@
-# LinguachatFrontend
+# LinguachatFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.5.
 
--- linguachat-front/src/app/app.component.spec.ts
+++ linguachat-front/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'linguachat-frontend'`, () => {
+  it(`should have as title 'linguachat-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('linguachat-frontend');
+    expect(app.title).toEqual('linguachat-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('linguachat-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('linguachat-front app is running!');
   });
 });
