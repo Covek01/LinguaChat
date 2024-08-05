70a872d4911ab0ac5f0852bceb8fcbabd98684c4
covek01 <stojiljkovic.nikola2001@gmail.com>
2024-08-05 12:38:28 +0200

    rezerva removed

--- rezerva/src/modules/language/language.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { LanguageService } from './language.service';
-
-@Controller('language')
-export class LanguageController {
-    constructor(private languageService: LanguageService){}
-
-}
