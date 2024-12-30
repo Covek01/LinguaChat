cbc2ff000627a0405d0d9e6f25ecc002082d542a
covek01 <stojiljkovic.nikola2001@gmail.com>
2024-12-30 18:18:58 +0100

    added language learning frontend

--- linguachat-frontend/src/modules/profile/myprofile/myprofile-body/myprofile-tab-languages/myprofile-tab-learning-languages/myprofile-tab-learning-languages.component.ts
+++ linguachat-frontend/src/modules/profile/myprofile/myprofile-body/myprofile-tab-languages/myprofile-tab-learning-languages/myprofile-tab-learning-languages.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { LanguageInterface } from 'src/models/language.types';
+import { selectLanguagesLearning } from 'src/store/user/languages-learning/languages-learning.selector';
 
 @Component({
   selector: 'app-myprofile-tab-learning-languages',
   templateUrl: './myprofile-tab-learning-languages.component.html',
-  styleUrls: ['./myprofile-tab-learning-languages.component.sass']
+  styleUrls: ['./myprofile-tab-learning-languages.component.sass'],
 })
 export class MyprofileTabLearningLanguagesComponent {
+  displayedColumns: string[] = ['name', 'popularity', 'actions'];
+  constructor(private readonly store: Store) {}
 
+  languagesLearning$ = this.store.select(selectLanguagesLearning);
 }
