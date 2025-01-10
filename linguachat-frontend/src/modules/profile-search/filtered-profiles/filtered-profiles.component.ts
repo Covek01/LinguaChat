f40f79874141e1ebad5231844a56d4634b5b5941
covek01 <stojiljkovic.nikola2001@gmail.com>
2025-01-10 16:39:47 +0100

    added frontend for filtered list

--- linguachat-frontend/src/modules/profile-search/filtered-profiles/filtered-profiles.component.ts
+++ linguachat-frontend/src/modules/profile-search/filtered-profiles/filtered-profiles.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectAllFilteredUsers, selectFilteredUsersState } from 'src/store/filtered-users/filtered-users.selector';
 
 @Component({
   selector: 'app-filtered-profiles',
   templateUrl: './filtered-profiles.component.html',
-  styleUrls: ['./filtered-profiles.component.sass']
+  styleUrls: ['./filtered-profiles.component.sass'],
 })
 export class FilteredProfilesComponent {
+  displayedColumns: string[] = ['username', 'born', 'country', 'city', 'since'];
+  constructor(private readonly store: Store){}
 
+  filteredUsers$ = this.store.select(selectAllFilteredUsers);
 }
